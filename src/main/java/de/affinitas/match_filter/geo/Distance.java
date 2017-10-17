c7d79950d1ca74e9ae53d2f4b379d8326d9a5ca6
Evgeniy Abduzhapparov <abd.evg@gmail.com>
2017-10-17 18:45:40 +0200

    Decouple the Distance class from the City class

--- src/main/java/de/affinitas/match_filter/geo/Distance.java
+++ src/main/java/de/affinitas/match_filter/geo/Distance.java
@@ -1,16 +1,14 @@
 package de.affinitas.match_filter.geo;
 
-import de.affinitas.match_filter.model.City;
-
 public class Distance {
 
     private static final int EARTH_RADIUS = 6371;
 
-    public static int getDistance(City city0, City city1) {
-        double lat0R = Math.toRadians(city0.getLat());
-        double lon0R = Math.toRadians(city0.getLon());
-        double lat1R = Math.toRadians(city1.getLat());
-        double lon1R = Math.toRadians(city1.getLon());
+    public static int getDistance(float lat0, float lon0, float lat1, float lon1) {
+        double lat0R = Math.toRadians(lat0);
+        double lon0R = Math.toRadians(lon0);
+        double lat1R = Math.toRadians(lat1);
+        double lon1R = Math.toRadians(lon1);
 
         double x = (lon0R - lon1R) * Math.cos((lat0R + lat1R) / 2);
         double y = lat0R - lat1R;
--- src/test/java/de/affinitas/match_filter/geo/DistanceTest.java
+++ src/test/java/de/affinitas/match_filter/geo/DistanceTest.java
@@ -1,50 +1,63 @@
 package de.affinitas.match_filter.geo;
 
-import de.affinitas.match_filter.model.City;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class DistanceTest {
 
-    private City berlin = new City("Berlin", 52.5164121f, 13.3774509f);
-    private City moscow = new City("Moscow", 55.7539303f, 37.6186063f);
-    private City munich = new City("Munich", 48.1354644f, 11.5738865f);
-    private City newYork = new City("New York", 40.7587857f, -73.9848582f);
-    private City potsdam = new City("Potsdam", 52.3998029f, 13.0360108f);
+    class Location {
+        private final float lat;
+        private final float lon;
+
+        Location(float lat, float lon) {
+            this.lat = lat;
+            this.lon = lon;
+        }
+    }
+
+    private Location berlin = new Location(52.5164121f, 13.3774509f);
+    private Location moscow = new Location(55.7539303f, 37.6186063f);
+    private Location munich = new Location(48.1354644f, 11.5738865f);
+    private Location newYork = new Location(40.7587857f, -73.9848582f);
+    private Location potsdam = new Location(52.3998029f, 13.0360108f);
 
     @Test
     public void testGetDistanceZero() {
-        assertEquals(0, Distance.getDistance(berlin, berlin));
+        assertEquals(0, getDistance(berlin, berlin));
     }
 
     @Test
     public void testGetDistanceBerlinPotsdam() {
-        assertEquals(26, Distance.getDistance(berlin, potsdam));
+        assertEquals(26, getDistance(berlin, potsdam));
     }
 
     @Test
     public void testGetDistanceBerlinMunich() {
-        assertEquals(503, Distance.getDistance(berlin, munich));
+        assertEquals(503, getDistance(berlin, munich));
     }
 
     @Test
     public void testGetDistanceBerlinMoscow() {
-        assertEquals(1619, Distance.getDistance(berlin, moscow));
+        assertEquals(1619, getDistance(berlin, moscow));
     }
 
     @Test
     public void testGetDistanceMoscowBerlin() {
-        assertEquals(1619, Distance.getDistance(moscow, berlin));
+        assertEquals(1619, getDistance(moscow, berlin));
     }
 
     @Test
     public void testGetDistanceBerlinNewYork() {
-        assertEquals(6796, Distance.getDistance(berlin, newYork));
+        assertEquals(6796, getDistance(berlin, newYork));
     }
 
     @Test
     public void testGetDistanceMoscowNewYork() {
-        assertEquals(8428, Distance.getDistance(moscow, newYork));
+        assertEquals(8428, getDistance(moscow, newYork));
+    }
+
+    private int getDistance(Location loc0, Location loc1) {
+        return Distance.getDistance(loc0.lat, loc0.lon, loc1.lat, loc1.lon);
     }
 }
